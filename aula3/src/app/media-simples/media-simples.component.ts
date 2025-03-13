c229ff921b08b90eaa715b44a9c15d454d08e997
Rafael Moreno <rafaelmorenosax@gmail.com>
2025-03-12 22:12:14 -0300

    Media simples

--- aula3/src/app/media-simples/media-simples.component.ts
+++ aula3/src/app/media-simples/media-simples.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-media-simples',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './media-simples.component.html',
   styleUrl: './media-simples.component.css'
 })
 export class MediaSimplesComponent {
+  nota1: number = 0;
+  nota2: number = 0;
+  nota3: number = 0;
+  nota4: number = 0;
+  media: number = 0;
+  situacao: string = '';
 
+  calcularMedia() {
+    this.media = (this.nota1 + this.nota2 + this.nota3 + this.nota4) / 4;
+    if (this.media >= 6)
+      this.situacao = 'Aprovado';
+    else
+      this.situacao = 'Reprovado';
+  }
 }
