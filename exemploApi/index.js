f59ed5a42f7fc6dffb4e92f03371bb4f0de8b706
Rafael Moreno <rafaelmorenosax@gmail.com>
2025-05-05 20:24:36 -0300

    adiciona métodos

--- exemploApi/index.js
+++ exemploApi/index.js
@@ -50,5 +50,37 @@ app.get('/produto', (req, res) => {
     res.json(produtos);
 });
 
+app.get('/produto/:id', (req, res) => {
+    const { id } = req.params;
+    const produto = produtos.find((p) => p.id == id);
+    if (!produto) {
+        return res.status(404).json({ error: 'Produto não encontrado' });
+    }
+    res.json(produto);
+});
+
+app.put('/produto/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, valor, temEstoque } = req.body;
+    const produto = produtos.find((p) => p.id == id);
+    if (!produto) {
+        return res.status(404).json({ error: 'Produto não encontrado' });
+    }
+    produto.nome = nome;
+    produto.valor = valor;
+    produto.temEstoque = temEstoque;
+    res.json(produto);
+});
+
+app.delete('/produto/:id', (req, res) => {
+    const { id } = req.params;
+    const index = produtos.findIndex((p) => p.id == id);
+    if (index === -1) {
+        return res.status(404).json({ error: 'Produto não encontrado' });
+    }
+    produtos.splice(index, 1);
+    res.status(204).send();
+});
+
 app.listen(3000, () =>
     console.log('Servidor rodando na porta: 3000'));
